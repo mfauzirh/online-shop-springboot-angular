70aeb2a192f0f5e1f416d7dbdb8d7332e379fa9a
mfauzirh <mfauzirizkih@gmail.com>
2024-09-04 14:38:25 +0700

    initialize backend project

---
+++ be-online-shop/src/main/java/com/mfauzirh/be_online_shop/BeOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.mfauzirh.be_online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOnlineShopApplication.class, args);
+	}
+
+}
---
+++ be-online-shop/src/test/java/com/mfauzirh/be_online_shop/BeOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfauzirh.be_online_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeOnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
