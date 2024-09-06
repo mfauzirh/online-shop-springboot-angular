433a932f8d6debcd188e4f932fa5f6c60bf862be
mfauzirh <mfauzirizkih@gmail.com>
2024-09-06 08:32:19 +0700

    success refactoring

--- fe-online-shop/src/app/customer/customer-list/customer-list.component.ts
+++ fe-online-shop/src/app/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 interface CustomerPreviewResponse {
   customerId: number;
@@ -13,33 +13,6 @@ interface CustomerPreviewResponse {
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
-export class CustomerListComponent implements OnInit{
-  customers: CustomerPreviewResponse[] = [];
-  page = 1;
-  pageSize = 5;
-
-  constructor() {
-    // Example data, replace with your actual data fetching logic
-    for (let i = 1; i <= 6; i++) {
-      this.customers.push({
-        customerId: i,
-        customerName: `Customer ${i}`,
-        customerAddress: `Address ${i}`,
-        customerCode: "13i21ioj412io4j12ijr",
-        pic: 'https://mdbootstrap.com/img/new/avatars/8.jpg' // Placeholder image
-      });
-    }
-  }
-
-  ngOnInit(): void {}
-
-  get paginatedCustomers(): CustomerPreviewResponse[] {
-    const start = (this.page - 1) * this.pageSize;
-    const end = start + this.pageSize;
-    return this.customers.slice(start, end);
-  }
-
-  onPageChange(page: number): void {
-    this.page = page;
-  }
+export class CustomerListComponent{
+  @Input() customers: CustomerPreviewResponse[] = [];
 }
--- fe-online-shop/src/app/customer/customer/customer.component.ts
+++ fe-online-shop/src/app/customer/customer/customer.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface CustomerPreviewResponse {
+  customerId: number;
+  customerName: string;
+  customerAddress: string;
+  customerCode: string;
+  pic: string;
+}
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit{
+  customers: CustomerPreviewResponse[] = [];
+  page = 1;
+  pageSize = 5;
 
+  constructor() {
+    // Example data, replace with your actual data fetching logic
+    for (let i = 1; i <= 20; i++) {
+      this.customers.push({
+        customerId: i,
+        customerName: `Customer ${i}`,
+        customerAddress: `Address ${i}`,
+        customerCode: "13i21ioj412io4j12ijr",
+        pic: 'https://mdbootstrap.com/img/new/avatars/8.jpg' // Placeholder image
+      });
+    }
+  }
+
+  ngOnInit(): void {}
+
+  get paginatedCustomers(): CustomerPreviewResponse[] {
+    const start = (this.page - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    return this.customers.slice(start, end);
+  }
+
+  onPageChange(page: number): void {
+    this.page = page;
+  }
 }
